fbbf22ea1fd0b1238aa394fba26797d2917708b1
Akhil Sarikonda <akhil.s@telco.in>
2023-05-08 12:17:15 +0100

    Replicated next config to match our environment

--- examples/with-next/next.config.js
+++ examples/with-next/next.config.js
@@ -1,6 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
+  images: {
+    domains: [
+      "images.ctfassets.net",
+    ],
+  },
+  css: {
+    optimize: true,
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            dimensions: false,
+          },
+        },
+      ],
+      type: "javascript/auto",
+      issuer: {
+        and: [/\.(js|ts)x?$/],
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
